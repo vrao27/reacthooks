0d00b611f9e2b16ec8e510c6393e4fa7fa12957c
oh-ve <maricic.ve@gmail.com>
2022-12-12 14:41:56 +0100

    homepage style updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import homeImage from "./images/homeImage.png";
+import homeImage from "./images/hooks.jpg";
 
 // function homeImage() {
 //   return <img src={homeImage} alt="use-state-img" />;
@@ -8,7 +8,7 @@ import homeImage from "./images/homeImage.png";
 function App() {
   return (
     <div className="App">
-      <h1>React Hooks</h1>
+      <h1>5 React Hooks you need to learn before you die</h1>
       <div className="home-content">
         <p>
           React hooks are a new feature in React 16.8 that allow you to use
@@ -21,17 +21,11 @@ function App() {
         <br />
         <img src={homeImage} alt="use-state-img" />
         <p>
-          In this example, the useState hook is used to add state to the
-          MyComponent function component. The useState hook returns an array
-          with two elements: the current value of the state, and a function to
-          update the state. In this case, the count variable contains the
-          current value of the state, and the setCount function is used to
-          update the state by incrementing the count by 1 when the button is
-          clicked. React provides several built-in hooks, including useState for
-          adding state to components, useEffect for running side effects in
-          function components, and useContext for accessing React context in
-          function components. You can also create your own custom hooks to
-          reuse stateful logic between components.
+          React provides several built-in hooks, including useState for adding
+          state to components, useEffect for running side effects in function
+          components, and useContext for accessing React context in function
+          components. You can also create your own custom hooks to reuse
+          stateful logic between components.
         </p>
       </div>
       <div id="buttoncontainer">
