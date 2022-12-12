4c04ff78875efe24a092755ca5a67005943e8a33
Vinay Rao <vrao8912@gmail.com>
2022-12-12 13:49:56 +0100

    callback data added

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React from "react";
+import callbackImage from "./images/callbackImage.jpg";
 
 export default function callbackhook() {
   return (
     <>
-    <div className='hook-header'>
-    <h1>React Hooks</h1>
-    <h2>useCallback</h2>
-    </div>
-    <div className='hook-content'>
-        
-    </div>
-   
+      <div className="hook-header">
+        <h1>React Hooks</h1>
+        <h2>useCallback</h2>
+      </div>
+      <div className="hook-content">
+        <p>
+          useCallback is a React hook that allows you to memoize a function.
+          This means that the function will only be recreated if one of its
+          dependencies has changed. This can be useful for optimizing
+          performance in React components that have expensive calculations or
+          operations that are triggered by React events.
+        </p>
+        <br />
+        <p>Here is an example of how useCallback can be used:</p>
+        <img src={callbackImage} alt="useCallback-img" />
+        <p>
+          In the code above, the handleClick function is defined using the
+          useCallback hook. The second argument to useCallback is an array of
+          dependencies, which specifies the values that the callback function
+          depends on. In this case, the handleClick function depends on the data
+          prop.
+          <br />
+          The handleClick function will only be recreated if the data prop
+          changes. This means that if the MyComponent component is re-rendered
+          with the same data prop, the handleClick function will be the same
+          reference as before and will not be recreated. This can help avoid
+          unnecessary calculations or operations and improve the performance of
+          the component.
+          <br />
+          When using useCallback, it is important to make sure that the
+          dependencies array is correct and up to date. If the dependencies
+          array is missing a value that the callback function actually depends
+          on, the function will not be recreated when it should, which can cause
+          bugs. On the other hand, if the dependencies array includes a value
+          that the callback function does not actually depend on, the function
+          will be recreated unnecessarily, which can also cause performance
+          issues.
+        </p>
+      </div>
     </>
   );
 }
