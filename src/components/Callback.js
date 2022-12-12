f2bb1bff4d141bcc7df35148491697e7cc1d756f
Vinay Rao <vrao8912@gmail.com>
2022-12-12 12:21:49 +0100

    home page content created

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-export default function componentName() {
+export default function callbackhook() {
   return (
     <>
-    
+    <div className='hook-header'>
+    <h1>React Hooks</h1>
+    <h2>useCallback</h2>
+    </div>
+    <div className='hook-content'>
+        
+    </div>
+   
     </>
   );
 }
